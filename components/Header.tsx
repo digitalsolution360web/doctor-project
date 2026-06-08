8d93ba0c8f35e755c7c12686e7880784ee69f078
Arun Verma <arunverma759954@gmail.com>
2026-06-08 16:37:51 +0530

    new updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -38,7 +38,9 @@ const Header = () => {
               <div className="w-6 h-6 rounded-full bg-teal-500/10 flex items-center justify-center mr-2 group-hover:bg-teal-500 transition-colors">
                 <Phone className="w-3 h-3 text-teal-400 group-hover:text-white" />
               </div>
+              +91-7895280742 ,
               +91-8826862154
+
             </a>
             <a href="mailto:info@midfloraherbal.com" className="flex items-center hover:text-teal-400 transition-all opacity-90 hover:opacity-100 group">
               <div className="w-6 h-6 rounded-full bg-teal-500/10 flex items-center justify-center mr-2 group-hover:bg-teal-500 transition-colors">
